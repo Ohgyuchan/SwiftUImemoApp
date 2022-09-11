b61c19ff67ab57de56c3d326de01d55430091239
Ohgyuchan <gyu3637@gmail.com>
2022-09-11 22:30:35 +0900

    Initial Commit

---
+++ Shared/SwiftUImemoAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUImemoAppApp.swift
+//  Shared
+//
+//  Created by Terman on 2022/09/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUImemoAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
