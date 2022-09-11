07ee6b637f561c04652926523dceeeb90dab7495
Ohgyuchan <gyu3637@gmail.com>
2022-09-11 22:34:45 +0900

    :art: Init directory setting

---
+++ Shared/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  SwiftUImemoApp
+//
+//  Created by Terman on 2022/09/11.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
